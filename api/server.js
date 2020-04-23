c7531cbe3ef9c1bc7a914284fee554ea763d12d3
Pergamene <smartz373@gmail.com>
2020-04-23 12:56:47 -0600

    server setup

---
+++ api/server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const Hobbits = require("../hobbits/hobbitsModel.js");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up" });
+});
+
+server.get("/hobbits", (req, res) => {
+  Hobbits.getAll()
+    .then(hobbits => {
+      res.status(200).json(hobbits);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+server.post("/hobbits", (req, res) => {
+  const hobbitInfo = req.body;
+
+  Hobbits.insert(hobbitInfo)
+    .then(ids => {
+      res.status(201).json({ message: "Hobbit created successfully" });
+    })
+    .catch(error => {
+      res.status(500).json({ errorMessage: error.message });
+    });
+});
+
+module.exports = server;
---
+++ api/server.test.js
@@ -0,0 +1,21 @@
+// const request = require('supertest');
+
+// const server = require('./server.js.js');
+
+// describe('sever', () => {
+//   describe('GET /', () => {
+//     it('should return 200 OK', async () => {
+//       const res = await request(server).get('/');
+//       expect(res.status).toBe(200);
+//     });
+//   });
+
+//   describe('POST /hobbits', () => {
+//     it.todo('return 201 on success', async () => {
+//       const res = await request(server).post('/hobbits').send({ name: 'Bilbo' });
+//       expect(res.status).toBe(200);
+//     });
+//     it.todo('add the hobbit to the db');
+//     it.todo('should return a message saying "Hobbit created successfully"');
+//   });
+// });

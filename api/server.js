b491885dcb0c27e0effd0ef93056137b36ef5b26
Pergamene <smartz373@gmail.com>
2020-04-23 17:22:38 -0600

    passing tests

--- api/server.js
+++ api/server.js
@@ -1,16 +1,16 @@
-const express = require("express");
+const express = require('express');
 
-const Hobbits = require("../hobbits/hobbitsModel.js");
+const Hobbits = require('../hobbits/hobbitsModel.js');
 
 const server = express();
 
 server.use(express.json());
 
-server.get("/", (req, res) => {
-  res.status(200).json({ api: "up" });
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'up' });
 });
 
-server.get("/hobbits", (req, res) => {
+server.get('/hobbits', (req, res) => {
   Hobbits.getAll()
     .then(hobbits => {
       res.status(200).json(hobbits);
@@ -20,12 +20,12 @@ server.get("/hobbits", (req, res) => {
     });
 });
 
-server.post("/hobbits", (req, res) => {
+server.post('/hobbits', (req, res) => {
   const hobbitInfo = req.body;
 
   Hobbits.insert(hobbitInfo)
     .then(ids => {
-      res.status(201).json({ message: "Hobbit created successfully" });
+      res.status(201).json({ message: 'Hobbit created successfully' });
     })
     .catch(error => {
       res.status(500).json({ errorMessage: error.message });
--- api/server.test.js
+++ api/server.test.js
@@ -1,21 +1,45 @@
-// const request = require('supertest');
+const request = require('supertest');
 
-// const server = require('./server.js.js');
+const server = require('./server');
+const db = require('../data/dbConfig');
 
-// describe('sever', () => {
-//   describe('GET /', () => {
-//     it('should return 200 OK', async () => {
-//       const res = await request(server).get('/');
-//       expect(res.status).toBe(200);
-//     });
-//   });
+describe('sever', () => {
+  describe('GET /', () => {
+    it('should return 200 OK', async () => {
+      const res = await request(server).get('/');
+      expect(res.status).toBe(200);
+    });
+  });
 
-//   describe('POST /hobbits', () => {
-//     it.todo('return 201 on success', async () => {
-//       const res = await request(server).post('/hobbits').send({ name: 'Bilbo' });
-//       expect(res.status).toBe(200);
-//     });
-//     it.todo('add the hobbit to the db');
-//     it.todo('should return a message saying "Hobbit created successfully"');
-//   });
-// });
+  describe("POST /hobbits", () => {
+    beforeEach(async () => {
+      await db("hobbits").truncate();
+    });
+
+    it("return 201 on success", async () => {
+      const res = await request(server).post("/hobbits").send({ name: "gaffer" });
+      expect(res.status).toBe(201);
+    });
+
+    it('should return a message saying "Hobbit created successfully"', async () => {
+      const res = await request(server).post("/hobbits").send({ name: "gaffer" })
+      expect(res.body.message).toBe("Hobbit created successfully");
+    });
+
+    it("add the hobbit to the db", async () => {
+      const hobbitName = "gaffer";
+
+      const existing = await db("hobbits").where({ name: hobbitName });
+      expect(existing).toHaveLength(0);
+
+      let res = await request(server).post("/hobbits").send({ name: hobbitName });
+      expect(res.body.message).toBe("Hobbit created successfully");
+
+      res = await request(server).post("/hobbits").send({ name: "sam" });
+      expect(res.body.message).toBe("Hobbit created successfully");
+
+      const inserted = await db("hobbits");
+      expect(inserted).toHaveLength(2);
+    });
+  });
+});
